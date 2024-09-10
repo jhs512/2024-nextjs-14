c19a1e224385e8597546255e61d6bbee72971423
jhs512 <jangka512@gmail.com>
2024-09-10 17:29:17 +0900

    work

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ export default function Home() {
     <div className="text-blue-500">
       <h1>홈 페이지!!!???123213213!!!</h1>
       <FontAwesomeIcon icon={faArrowAltCircleRight} />
+      <img src="/img/logo_1.jpg" />
+      <video src="/video/video_1.mp4" autoPlay muted loop />
     </div>
   );
 }
