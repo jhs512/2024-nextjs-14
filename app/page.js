4e3b6d3b88bc707822685f753b266703a707444c
jhs512 <jangka512@gmail.com>
2024-09-10 17:47:39 +0900

    work

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons/faArrowAltCircleRight";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="text-blue-500">
       <h1>홈 페이지!!!???123213213!!!</h1>
       <FontAwesomeIcon icon={faArrowAltCircleRight} />
+      <Image width={200} height={300} src="/img/logo_1.jpg" />
       <img src="/img/logo_1.jpg" />
       <video src="/video/video_1.mp4" autoPlay muted loop />
     </div>
